1ee9c3808f59bcf315f1da675bc7305f9a975a01
Sarvar17 <lonelyswordsman17@gmail.com>
2021-12-13 23:45:53 +0300

    Add project files.

---
+++ Task1/Functions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+
+namespace Task1
+{
+    class Functions
+    {
+        /// <summary>
+        /// Method to count number of words in text.
+        /// </summary>
+        /// <param name="text"> Text. </param>
+        /// <returns></returns>
+        public static int countWords(string text)
+        {
+            return text.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        /// <summary>
+        /// Method to count number of vowels in text.
+        /// </summary>
+        /// <param name="text"> Text. </param>
+        /// <returns></returns>
+        public static int countVowel(string text)
+        {
+            // Initializing english and russian vowels.
+            char[] vowels = { 'a', 'e', 'u', 'i', 'o', 'а', 'о', 'у', 'ы', 'э', 'я', 'ё', 'ю', 'и', 'е' };
+
+            return text.ToLower().Count(x => vowels.Contains(x));
+        }
+
+        /// <summary>
+        /// Method to get response of site;
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static T getResponse<T>(int id)
+        {
+            using (var wc = new WebClient())
+            {
+                var json_data = string.Empty;
+                try
+                {
+                    // Adding a header.
+                    wc.Headers.Add("TMG-Api-Key", "0J/RgNC40LLQtdGC0LjQutC4IQ==");
+                    // Downloading json string.
+                    json_data = wc.DownloadString($"http://tmgwebtest.azurewebsites.net/api/textstrings/{id}");
+                }
+                catch (Exception e)  // Catching exception.
+                {
+                    throw new ArgumentException(e.Message);
+                }
+
+                // returning deserialized json data if it is not null or empty.
+                return JsonSerializer.Deserialize<T>(json_data);
+            }
+        }
+
+        /// <summary>
+        /// Method to make one line multiline;
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string makeMultiline(string text)
+        {
+            string result = string.Empty;
+
+            while (text.Length > 55)
+            {
+                result += text.Substring(0, 55) + "\n";
+                text = text.Substring(55, text.Length - 55);
+            }
+
+            result += text;
+
+            return result;
+        }
+    }
+}
---
+++ Task1/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Method to calculate number of words and vowels when button clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            calculateAllInputedIDs();
+        }
+
+        /// <summary>
+        /// Method to count words and vowels of first text when text in first text box inputted.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void firstTextBox_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            calculateFirst();
+        }
+
+        /// <summary>
+        /// Method to count words and vowels of second text when text in second text box inputted.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void secondTextBox_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            calculateSecond();
+        }
+
+        /// <summary>
+        /// Method to count words and vowels of third text when text in third text box inputted.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void thirdTextBox_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            calculateThird();
+        }
+
+        // Method to count words and vowels of second text when text in second text box inputted.
+        private void calculateFirst()
+        {
+            firstWordsNum.Content = Functions.countWords(firstTextBox.Text);
+            firstVowelsNum.Content = Functions.countVowel(firstTextBox.Text);
+        }
+
+        // Method to count words and vowels of second text when text in second text box inputted.
+        private void calculateSecond()
+        {
+            secondWordsNum.Content = Functions.countWords(secondTextBox.Text);
+            secondVowelsNum.Content = Functions.countVowel(secondTextBox.Text);
+        }
+
+        // Method to count words and vowels of third text when text in third text box inputted.
+        private void calculateThird()
+        {
+            thirdWordsNum.Content = Functions.countWords(thirdTextBox.Text);
+            thirdVowelsNum.Content = Functions.countVowel(thirdTextBox.Text);
+        }
+
+        // Changing values method;
+        private void Change(int i, Data data)
+        {
+            switch (i)
+            {
+                case 0:
+                    firstTextBox.Text = Functions.makeMultiline(data.text);
+                    calculateFirst();
+                    break;
+                case 1:
+                    secondTextBox.Text = Functions.makeMultiline(data.text);
+                    calculateSecond();
+                    break;
+                case 2:
+                    thirdTextBox.Text = Functions.makeMultiline(data.text);
+                    calculateThird();
+                    break;
+            }
+        }
+
+        // Method to calculate inputted IDs
+        private void calculateAllInputedIDs()
+        {
+            string[] ids = idTextBox.Text.Split(',', ';', StringSplitOptions.RemoveEmptyEntries);
+            if (ids.Length < 0 || ids.Length > 3)
+            {
+                MessageBox.Show("Number of IDs have to be from 0 to 3 and splitted with ',' or ';'", "Error");
+                return;
+            }
+            else if (ids.Length == 0)
+            {
+                calculateFirst();
+                calculateSecond();
+                calculateThird();
+            }
+            else
+            {
+                for (int i = 0; i < ids.Length; i++)
+                {
+                    if (int.TryParse(ids[i], out int temp))
+                    {
+                        Data data;
+                        try
+                        {
+                            data = Functions.getResponse<Data>(temp);
+                        }
+                        catch (Exception exception)
+                        {
+                            MessageBox.Show(exception.Message, "Error");
+                            return;
+                        }
+                        Change(i, data);
+                    }
+                    else
+                    {
+                        MessageBox.Show("IDs must be a number and from 1 to 20", "Error");
+                    }
+                }
+            }
+        }
+    }
+}
